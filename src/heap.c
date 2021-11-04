5104e9ba76c1665f7ac2dbe3faeb11966c4d881a
m1cha1s <msulej04@gmail.com>
2021-11-04 19:21:02 +0100

    Malloc done (temporarely)

--- src/heap.c
+++ src/heap.c
@@ -25,7 +25,7 @@ void* malloc(unsigned long long size)
     {
         if(current_memory_segment->memory_lenght >= size)
         {
-            if(current_memory_segment->memory_lenght != size)
+            if(current_memory_segment->memory_lenght > size + sizeof(struct memory_segment_header))
             {
                 struct memory_segment_header* new_segment_header = (struct memory_segment_header*)((unsigned long long)current_memory_segment + sizeof(struct memory_segment_header) + size);
 
@@ -38,13 +38,27 @@ void* malloc(unsigned long long size)
 
                 current_memory_segment->next_free_segment = new_segment_header;
                 current_memory_segment->next_segment = new_segment_header;
+                current_memory_segment->memory_lenght = size;
             }
             if(current_memory_segment == first_free_memory_segment)
             {
                 first_free_memory_segment = current_memory_segment->next_free_segment;
             }
+            current_memory_segment->free = 0;
+            if(current_memory_segment->previous_free_segment != 0) current_memory_segment->previous_free_segment->next_free_segment = current_memory_segment->next_free_segment;
+            if(current_memory_segment->next_free_segment != 0) current_memory_segment->next_free_segment->previous_free_segment = current_memory_segment->previous_free_segment;
+            if(current_memory_segment->previous_free_segment != 0) current_memory_segment->previous_free_segment->next_free_segment = current_memory_segment->next_free_segment;
+            if(current_memory_segment->next_free_segment != 0) current_memory_segment->next_free_segment->previous_free_segment = current_memory_segment->previous_free_segment;
+
+            if(current_memory_segment->previous_segment != 0) current_memory_segment->previous_segment->next_free_segment = current_memory_segment->next_free_segment;
+            if(current_memory_segment->next_segment != 0) current_memory_segment->next_segment->previous_free_segment = current_memory_segment->previous_free_segment;
 
             return current_memory_segment + sizeof(struct memory_segment_header);
         }
+        if(current_memory_segment->next_free_segment == 0)
+        {
+            return 0;
+        }
+        current_memory_segment = current_memory_segment->next_free_segment;
     }
 }
--- src/kernel.c
+++ src/kernel.c
@@ -17,13 +17,21 @@ extern void main()
     find_usable_memory_regions();
 
     heap_init(0x100000, 0x100000);
-    void* test_memory_addr = malloc(60);
+
+    void* test_memory_addr_0 = malloc(0x10);
+    void* test_memory_addr_1 = malloc(0x10);
+    void* test_memory_addr_2 = malloc(0x10);
 
     putsat(numToStr(usable_memory_region_count, 10), 0, 0);
     SetCursorPosition(PositionFromCords(0, 1)) ;
     puts("Welcome to OS_0!\n\n");
 
-    puts(numToStr((unsigned long long)test_memory_addr, 10));
+    puts(numToStr((unsigned long long)test_memory_addr_0, 10));
+    puts("\n");
+    puts(numToStr((unsigned long long)test_memory_addr_1, 10));
+    puts("\n");
+    puts(numToStr((unsigned long long)test_memory_addr_2, 10));
+    puts("\n");
 
     __asm__ __volatile__ ("sti");
 
